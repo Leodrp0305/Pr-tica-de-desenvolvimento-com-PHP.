8fa941b65aae0a53cdf6e2864cb3eb3f81adc4ea
Leonardo <leokenjits@gmail.com>
2025-08-29 17:59:11 -0300

    Exercícios 27 e 28
    Para melhor contextualização, eu faço os códigos a partir do momento que tenho tempo livre, por tanto, os envio picotado

---
+++ Exercicio27.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+Data: 29 de Agosto de 2025
+Descritivo: Teste se uma variável é maior que 100 OU menor que 50
+*******************************************************************************/
+$Num1 = rand(0, 150);
+if ($Num1 > 100 || $Num1 <50) {
+ echo $Num1 . " é maior que 100 ou menor que 50";
+} else {
+ echo $Num1 . " não é maior que 100 ou menor que 50";
+}
+
+?>
---
+++ Exercicio28.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+Data: 29 de Agosto de 2025
+Descritivo: Verifique se um número é par E positivo
+*******************************************************************************/
+$Num1 = rand(-100, 100);
+
+if ( $Num1 % 2 == 0 || $Num1 > 0) {
+    if ( $Num1 % 2 == 0 && $Num1 > 0){
+    echo $Num1 . " é par e positivo";
+    } elseif ($Num1 % 2 == 0) {
+        echo $Num1 . " é par";
+    } elseif ($Num1 > 0) {
+        echo $Num1 . " é positivo";
+    }
+} else {
+    echo $Num1 . " não é par nem positivo";
+}
+?>
