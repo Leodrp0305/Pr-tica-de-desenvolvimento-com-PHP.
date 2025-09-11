bf0ace8022e03bb94b229da0557cd49e4cb357f0
Leonardo <leokenjits@gmail.com>
2025-09-11 19:07:25 -0300

    Update Exercicio02.php

--- Exercicio02.php
+++ Exercicio02.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Crie um script que subtraia 15 de 45 e mostre o resultado
 *******************************************************************************/
    echo (45-15) ." é o Resultado de 45 - 15"; //eu boto o echo pra poder mostrar o resultado
    
 ?>
+
