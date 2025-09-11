362f671eac16b1d0148bdb6afa64b22fab9ff83a
Leonardo <leokenjits@gmail.com>
2025-09-11 19:08:42 -0300

    Update Exercicio03.php

--- Exercicio03.php
+++ Exercicio03.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Faça um programa que multiplique 8 por 7 e divida por 2
 *******************************************************************************/
    echo ((8*7)/2)." é o Resultado de (8*7)/2"; //eu boto o echo pra poder mostrar o resultado
    
 ?>
+
