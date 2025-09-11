a6805d234abceaeb9b21ee6c6786906dc925852c
Leonardo <leokenjits@gmail.com>
2025-09-11 19:06:36 -0300

    Update Exercicio01.php

--- Exercicio01.php
+++ Exercicio01.php
@@ -6,6 +6,9 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
+ 
 Data: 28 de Agosto de 2025
 Descritivo: Escreva um programa que calcule a soma de dois números
 *******************************************************************************/
@@ -15,3 +18,4 @@ Descritivo: Escreva um programa que calcule a soma de dois números
    $soma = $Num1 + $Num2;
    echo $soma . " é o resultado da soma";
 ?>
+
