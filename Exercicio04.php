769d0bac5874be2fd25f5d30f54948951f5b40bd
Leonardo <leokenjits@gmail.com>
2025-09-11 19:09:04 -0300

    Update Exercicio04.php

--- Exercicio04.php
+++ Exercicio04.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Escreva um código que calcule o resto da divisão de 17 por 5
 *******************************************************************************/
    echo (17%5)." é o resto de 17 por 5"; //eu boto o echo pra poder mostrar o resultado
    
 ?>
+
