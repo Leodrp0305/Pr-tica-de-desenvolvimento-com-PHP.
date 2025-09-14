95e0b1d93af2dc50950be4fded8e46ebb5747686
Leonardo <leokenjits@gmail.com>
2025-09-14 10:34:29 -0300

    Update Exercicio08.php

--- Exercicio08.php
+++ Exercicio08.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Crie um código que calcule a área de um círculo (πr²)
 *******************************************************************************/
@@ -13,3 +15,4 @@ Descritivo: Crie um código que calcule a área de um círculo (πr²)
  $Area = pi()* $Raio**2;
  echo $Area;
 ?>
+
