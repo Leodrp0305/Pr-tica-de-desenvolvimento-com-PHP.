b897230aa88269344adf5c1b4a94d0927c6b5694
Leonardo <leokenjits@gmail.com>
2025-09-14 10:34:55 -0300

    Update Exercicio09.php

--- Exercicio09.php
+++ Exercicio09.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Faça um programa que calcule o aumento de 15% sobre um valor
 *******************************************************************************/
@@ -15,3 +17,4 @@ Descritivo: Faça um programa que calcule o aumento de 15% sobre um valor
  echo "Valor com 15% a mais ". $Resultado;
 
 ?>
+
