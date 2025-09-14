dd87bcfee6b5fa04683696183fb1e7a230cbc7f4
Leonardo <leokenjits@gmail.com>
2025-09-14 10:38:14 -0300

    Update Exercicio13.php

--- Exercicio13.php
+++ Exercicio13.php
@@ -6,10 +6,13 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Faça um script que use *= para dobrar o valor de uma variável
 *******************************************************************************/
  $Variavel_dobrar = 3;
  $Variavel_dobrar *= 2;
  echo "Esse é o dobro da variavel inicial = ".$Variavel_dobrar;
+
 ?>
