958087bafb3426374c002a738d09a0e41ea5a237
Leonardo <leokenjits@gmail.com>
2025-09-14 10:37:44 -0300

    Update Exercicio12.php

--- Exercicio12.php
+++ Exercicio12.php
@@ -6,10 +6,13 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Crie um programa que use += para adicionar 10 a uma variável
 *******************************************************************************/
  $Valor = 100;
  $Valor += 10;
  echo $Valor;
+
 ?>
