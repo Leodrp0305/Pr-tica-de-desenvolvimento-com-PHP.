60c7d9aa5fbfd8aa5891774749ae859e271bccc9
Leonardo <leokenjits@gmail.com>
2025-09-14 10:33:48 -0300

    Update Exercicio07.php

--- Exercicio07.php
+++ Exercicio07.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Escreva um programa que converta graus Celsius para Fahrenheit (F = C × 9/5 + 32)
 *******************************************************************************/
@@ -13,3 +15,4 @@ Descritivo: Escreva um programa que converta graus Celsius para Fahrenheit (F = 
   $Fahrenheit = $Celsius * 9/5 + 32;
   echo $Celsius . " Em Fahrenheit é " . $Fahrenheit;
 ?>
+
