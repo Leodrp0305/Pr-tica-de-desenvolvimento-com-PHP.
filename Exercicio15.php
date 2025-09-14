4b2ce0cb13bb51ad9dbf8062679caccd8301439e
Leonardo <leokenjits@gmail.com>
2025-09-14 10:41:51 -0300

    Update Exercicio15.php

--- Exercicio15.php
+++ Exercicio15.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Crie um programa que use %= para armazenar o resto de uma divisão
 *******************************************************************************/
@@ -13,4 +15,5 @@ Descritivo: Crie um programa que use %= para armazenar o resto de uma divisão
  $Variavel_resto %= 6;
  echo "Esse é o resto da variavel inicial = ".$Variavel_resto; 
  // mesmo codigo do ex 13, mas utiliza do % para tirar o resto
+
 ?>
