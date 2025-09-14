0b72e1086940dfe7a04f3d8e5cd998524f40eea7
Leonardo <leokenjits@gmail.com>
2025-09-14 10:32:17 -0300

    Update Exercicio05.php

--- Exercicio05.php
+++ Exercicio05.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Crie um programa que eleve 4 à potência de 3
 *******************************************************************************/
    echo (4**3)." é o resultadode 4 elevado a terceira"; //eu boto o echo pra poder mostrar o resultado
    
 ?>
+
