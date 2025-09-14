9288298e98d9abcfbb3656ebf364b50ac609d1ce
Leonardo <leokenjits@gmail.com>
2025-09-14 10:36:46 -0300

    Update Exercicio11.php

--- Exercicio11.php
+++ Exercicio11.php
@@ -6,10 +6,13 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Use operadores de atribuição para incrementar uma variável em 5
 *******************************************************************************/
  $Valor = 20;
  $Valor += 5;
  echo $Valor;
+
 ?>
