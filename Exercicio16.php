f26740f591c9543c82799a8116c07a86f6cc75d0
Leonardo <leokenjits@gmail.com>
2025-09-14 10:42:42 -0300

    Update Exercicio16.php

--- Exercicio16.php
+++ Exercicio16.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Verifique se 10 é maior que 5
 *******************************************************************************/
@@ -18,4 +20,5 @@ Descritivo: Verifique se 10 é maior que 5
  } else {
     echo "Algo errado aconteceu";
  }
+
 ?>
