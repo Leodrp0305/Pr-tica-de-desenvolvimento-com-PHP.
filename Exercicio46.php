ed8f21822fe2696addc83565e10d0151ae8d667d
Leonardo <leokenjits@gmail.com>
2025-09-15 15:06:37 -0300

    Update Exercicio46.php

--- Exercicio46.php
+++ Exercicio46.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Crie uma expressão ternária que retorne "Maioridade" ou "Menoridade" baseado na idade
 *******************************************************************************/
 $idade = 17;
 echo ($idade) ? "Maioridade" : "Menoridade";
+
     ?>
