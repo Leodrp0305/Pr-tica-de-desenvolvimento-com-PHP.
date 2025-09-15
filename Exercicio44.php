0a45afccb6d2a565d66c35e2959ccecd552e158a
Leonardo <leokenjits@gmail.com>
2025-09-15 15:05:36 -0300

    Update Exercicio44.php

--- Exercicio44.php
+++ Exercicio44.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Crie um ternário que retorne "Aprovado" se nota >= 7, senão "Reprovado
 *******************************************************************************/
  $nota = 7;
  echo ($nota >= 7) ? "Aprovado" : "Reprovado, boa sorte na próxima";
+
     ?>
