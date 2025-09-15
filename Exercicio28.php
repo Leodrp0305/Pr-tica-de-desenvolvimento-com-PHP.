f038bda5e5143292e4c8b0b495b8916a93d538e5
Leonardo <leokenjits@gmail.com>
2025-09-15 14:48:06 -0300

    Update Exercicio28.php

--- Exercicio28.php
+++ Exercicio28.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 29 de Agosto de 2025
 Descritivo: Verifique se um número é par E positivo
 *******************************************************************************/
@@ -22,4 +24,5 @@ if ( $Num1 % 2 == 0 || $Num1 > 0) {
 } else {
     echo $Num1 . " não é par nem positivo";
 }
+
 ?>
