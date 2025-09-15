517ec18ae59b7548d10e9418ceacbf297ecaf70a
Leonardo <leokenjits@gmail.com>
2025-09-15 15:06:15 -0300

    Update Exercicio45.php

--- Exercicio45.php
+++ Exercicio45.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Use operador ternário para verificar se uma string está vazia
 *******************************************************************************/
 $String_qualquer = "Eu amo minha namorada";
 echo ($String_qualquer > 0) ? "A string não está vazia" : "A string está vazia";
+
     ?>
