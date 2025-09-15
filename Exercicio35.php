8c647b85c4a2d355d957d265225faaa8234bed7c
Leonardo <leokenjits@gmail.com>
2025-09-15 14:59:37 -0300

    Update Exercicio35.php

--- Exercicio35.php
+++ Exercicio35.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Faça um programa que mostre mensagem diferente para manhã, tarde e noite
 *******************************************************************************/
@@ -20,4 +22,5 @@ date_default_timezone_set("America/Sao_Paulo");
  } else {
     echo "Vai dormir, olha o horário é " . $Horario . " já";
  }
+
 ?>
