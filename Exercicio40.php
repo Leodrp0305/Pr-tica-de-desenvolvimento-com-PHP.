b94cbeca0d0bad45cb1356c02fc4ac5ce8e50a26
Leonardo <leokenjits@gmail.com>
2025-09-15 15:03:57 -0300

    Update Exercicio40.php

--- Exercicio40.php
+++ Exercicio40.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Crie uma calculadora simples com switch case
 *******************************************************************************/
@@ -36,5 +38,6 @@ switch ($calculo) {
     echo "Operação desconhecida";
     break;
 }
+
 
     ?>
