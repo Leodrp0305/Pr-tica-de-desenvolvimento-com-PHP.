0d85ef5cf3d21da640d74829f5cefcb7381c8755
Leonardo <leokenjits@gmail.com>
2025-09-15 14:51:31 -0300

    Update Exercicio31.php

--- Exercicio31.php
+++ Exercicio31.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 29 de Agosto de 2025
 Descritivo: Crie uma condição que verifique se uma pessoa é maior de idade
 *******************************************************************************/
@@ -15,5 +17,6 @@ if ($Idade >= 18) {
 } else {
    echo "Você é menor de idade";
 }
+
 
 ?>
