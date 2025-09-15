4727a5fb3bd74f5a859137f4c02a56605b9266f3
Leonardo <leokenjits@gmail.com>
2025-09-15 15:06:52 -0300

    Update Exercicio47.php

--- Exercicio47.php
+++ Exercicio47.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Use ternário para determinar se um número é múltiplo de 5
 *******************************************************************************/
 $num1 = rand(0, 100);
 echo ($num1%5 == 0)? "$num1 é multiplo de 5" : "$num1 não é multiplo de 5";
+
     ?>
