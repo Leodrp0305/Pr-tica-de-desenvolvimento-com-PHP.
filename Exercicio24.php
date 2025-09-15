a94756d88124bd29268a45d4ede1ac766e609bdb
Leonardo <leokenjits@gmail.com>
2025-09-15 10:57:58 -0300

    Update Exercicio24.php

--- Exercicio24.php
+++ Exercicio24.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 29 de Agosto de 2025
 Descritivo: Teste se uma variável é diferente de null
 *******************************************************************************/
@@ -19,3 +21,4 @@ if ($Variavel_Qualquer !== $nulo) {
 }
 
 ?>
+
