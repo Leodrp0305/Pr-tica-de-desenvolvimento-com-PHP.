81dd13c9984285b8d084a70bd7c7e21dc62e26cb
Leonardo <leokenjits@gmail.com>
2025-09-15 15:04:59 -0300

    Update Exercicio42.php

--- Exercicio42.php
+++ Exercicio42.php
@@ -6,10 +6,13 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Crie uma expressão ternária que retorne "Par" ou "Ímpar" baseado em um número
 *******************************************************************************/
 $num_exemplar = 11; //eu gosto de usar rand()nesse caso, mas vou fazer normal por que sim.
 
 echo ($num_exemplar%2 == 0) ? "$num_exemplar é par": "$num_exemplar é impar";
+
     ?>
