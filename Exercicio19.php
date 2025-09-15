bbcab704dcd24f337680e00b018bfa308de5bfd3
Leonardo <leokenjits@gmail.com>
2025-09-15 10:54:21 -0300

    Update Exercicio19.php

--- Exercicio19.php
+++ Exercicio19.php
@@ -6,10 +6,13 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Compare se um número é diferente de outro
 *******************************************************************************/
  $num1 = 3;
  $num2 = 5;
  echo ($num1 != $num2) ?"São diferentes as váriaveis":"As variaveis são iguais";
+
 ?>
