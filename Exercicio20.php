36452410e9aa3c643128cd50a1e6a78ac90a7376
Leonardo <leokenjits@gmail.com>
2025-09-15 10:55:28 -0300

    Update Exercicio20.php

--- Exercicio20.php
+++ Exercicio20.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Verifique se uma variável é maior ou igual a 100
 *******************************************************************************/
@@ -17,5 +19,6 @@ Descritivo: Verifique se uma variável é maior ou igual a 100
  } else {
     echo "Error";
  }
+
 
 ?>
