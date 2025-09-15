1cc9795d01c61f0f74ba42ebf2dd158be8d22faf
Leonardo <leokenjits@gmail.com>
2025-09-15 10:57:12 -0300

    Update Exercicio23.php

--- Exercicio23.php
+++ Exercicio23.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 29 de Agosto de 2025
 Descritivo: Verifique se 0 é idêntico a false
 *******************************************************************************/
@@ -20,3 +22,4 @@ if ($Zero === $falso) {
 
 
 ?>
+
