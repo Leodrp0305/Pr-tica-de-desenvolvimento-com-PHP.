ceb610d4cf2859f076c8705c00c55fe887bf4765
Leonardo <leokenjits@gmail.com>
2025-09-15 10:53:53 -0300

    Update Exercicio18.php

--- Exercicio18.php
+++ Exercicio18.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 28 de Agosto de 2025
 Descritivo: Verifique se duas variáveis são idênticas em valor e tipo
 *******************************************************************************/
@@ -13,4 +15,5 @@ Descritivo: Verifique se duas variáveis são idênticas em valor e tipo
  $Varaivel2 = "12";
 
  echo ($Variavel1 === $Varaivel2) ? "Ambas as váriaveis são identicas em tipo e valor":"Não são identicas";
+
 ?>
