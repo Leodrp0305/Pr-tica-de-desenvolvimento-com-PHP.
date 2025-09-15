80fb495b9416c92238876656b8298840c957b166
Leonardo <leokenjits@gmail.com>
2025-09-15 15:05:17 -0300

    Update Exercicio43.php

--- Exercicio43.php
+++ Exercicio43.php
@@ -6,9 +6,12 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Use ternário para verificar se uma pessoa pode votar (idade >= 16)
 *******************************************************************************/
  $idade = 17;
  echo ($idade >= 16) ? "Com $idade você pode votar" : "Nenem de $idade anos não pode votar";
+
     ?>
