cc195a277bc340ce98ff21f24a37a4b6149d02c9
Leonardo <leokenjits@gmail.com>
2025-09-15 10:55:50 -0300

    Update Exercicio21.php

--- Exercicio21.php
+++ Exercicio21.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 29 de Agosto de 2025
 Descritivo: Teste se uma string é menor que outra em ordem alfabética
 *******************************************************************************/
@@ -15,3 +17,4 @@ $palavra_Dois = "Abacaxi";
 echo ($palavra_Um < $palavra_Dois) ? $palavra_Um . " é antes no alfabeto que " . $palavra_Dois:$palavra_Um . " é depois no alfabeto que " . $palavra_Dois;
 
 ?>
+
