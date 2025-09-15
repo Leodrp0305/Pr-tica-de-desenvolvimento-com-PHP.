365cdea4bc97151f5f9d571c75936c99e38d0821
Leonardo <leokenjits@gmail.com>
2025-09-15 15:08:03 -0300

    Update Exercicio48.php

--- Exercicio48.php
+++ Exercicio48.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 01 de Setembro de 2025
 Descritivo: Crie um ternário que retorne "Frio" se temperatura < 15, "Agradável" se entre 15-25, "Quente" se >
 25
@@ -13,4 +15,5 @@ Descritivo: Crie um ternário que retorne "Frio" se temperatura < 15, "Agradáve
 $temperatura = 27;
 echo ($temperatura < 15)? "Frio":
     (($temperatura >=15 && $temperatura <25) ? "Agradável":"Quente");
+
     ?>
