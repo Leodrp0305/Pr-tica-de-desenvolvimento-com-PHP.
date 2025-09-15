2f50f8dcac2a6d08c05c04b9a24525821c7b13b1
Leonardo <leokenjits@gmail.com>
2025-09-15 14:52:20 -0300

    Update Exercicio33.php

--- Exercicio33.php
+++ Exercicio33.php
@@ -6,6 +6,8 @@ Professor: Flores
 Turma: ESOFT-2A
 Componentes:
  25185655-2 - Leonardo Kenji Tanida Soares
+ 25011023-2 - Lucas Coelho Suero
+ 25125961-2 - Elias Borges Neckel
 Data: 29 de Agosto de 2025
 Descritivo: Escreva um script que determine se um número é par ou ímpar
 *******************************************************************************/
@@ -15,4 +17,5 @@ Descritivo: Escreva um script que determine se um número é par ou ímpar
 } else {
  echo $num . " é impar";
 }
+
 ?>
